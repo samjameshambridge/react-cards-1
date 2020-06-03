5246837bf1da2d365be40c555cdcef34981e630d
Sam Hambridge <samhambridge@waste-creative.com>
2020-06-03 11:03:57 +0100

    commit

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -35,14 +35,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
-          },
-        ],
+        // use: [
+        //   {
+        //     loader: "babel-loader",
+        //     options: {
+        //       presets: ["@babel/preset-env", "@babel/preset-react"],
+        //     },
+        //   },
+        // ],
       },
       // {
       //   test: /\.css$/,
