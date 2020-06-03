aea2323a72e5c0da06ad39dedc2feeea0022eab6
Sam Hambridge <samhambridge@waste-creative.com>
2020-06-03 11:11:32 +0100

    commit

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,5 +1,5 @@
-// const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
@@ -11,43 +11,43 @@ module.exports = {
     filename: "js/bundle.js",
   },
   plugins: [
-    // new MiniCssExtractPlugin({
-    //   filename: "css/style.css",
-    // }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
     new webpack.ProvidePlugin({
       gsap: "gsap",
     }),
-    // new HtmlWebPackPlugin({
-    //   cache: false,
-    //   filename: "index.html",
-    //   inject: true,
-    //   minify: true,
-    //   meta: {
-    //     content:
-    //       "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no",
-    //   },
-    //   template: "./app/html-template/index.html",
-    //   title: "React Cards",
-    // }),
+    new HtmlWebPackPlugin({
+      cache: false,
+      filename: "index.html",
+      inject: true,
+      minify: true,
+      meta: {
+        content:
+          "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no",
+      },
+      template: "./app/html-template/index.html",
+      title: "React Cards",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        // use: [
-        //   {
-        //     loader: "babel-loader",
-        //     options: {
-        //       presets: ["@babel/preset-env", "@babel/preset-react"],
-        //     },
-        //   },
-        // ],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
       },
-      // {
-      //   test: /\.css$/,
-      //   use: [MiniCssExtractPlugin.loader, "css-loader"],
-      // },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
   },
 };
